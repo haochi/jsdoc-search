1f5e1b72e2aa3f3871918bf53bde69262bf9df7e
Haochi Chen <haochi@insidevault.com>
2015-05-15 03:42:55 -0700

    Add gh-pages

---
+++ src/test.js
@@ -0,0 +1,17 @@
+/**
+ * @author haochi
+ * @since May 15, 2015
+ */
+class Test {
+  /**
+   * This is a constructor
+   */
+  constructor() {
+  }
+
+  /**
+   * You can find me!!
+   */
+  search() {
+  }
+}
--- statics/scripts/angular.jsdoc.search.js
+++ statics/scripts/angular.jsdoc.search.js
@@ -8,21 +8,28 @@ angular
 
       me.search = function (selected) {
           var doc,
-              path;
+              path,
+              url;
 
           if (selected) {
               doc = selected.originalObject;
               me.src.forEach(function (src) {
+                  if (src[src.length-1] === '/') {
+                    src = src.slice(0, src.length-1);
+                  }
+
                   if (doc.meta.path.indexOf(src) === 0) {
                       path = doc.meta.path.slice(src.length);
                   }
               });
 
-              if (path) {
-                $window.location = path.split('/')
+              if (path !== undefined) {
+                url = '/' + path.split('/')
                                        .concat([doc.meta.filename])
                                        .filter(function (p) { return p; })
                                        .join('_')  + '.html#line' + doc.meta.lineno;
+
+                $window.location = url;
               }
           }
       };
